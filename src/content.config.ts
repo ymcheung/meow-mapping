55a7d83b7ac81d4cc8e5b9fd836317ab4345c14a
ymcheung <ymcheung.tw@me.com>
2024-12-21 00:22:02 +0800

    Update time

---
+++ src/content.config.ts
@@ -0,0 +1,30 @@
+import { z, defineCollection } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const postsCollection = defineCollection({
+  loader: glob({ pattern: '**/[^_]*.mdx', base: './src/data/posts' }),
+  schema: z.object({
+    title: z.string(),
+    description: z.string(),
+    difficulty: z.object({
+      level: z.enum(['easy', 'medium', 'hard'])
+    }),
+    feed: z.boolean(),
+    transportation: z.object({
+      duration: z.number(),
+      from: z.object({
+        place: z.string(),
+        kanji: z.string()
+      }),
+      by: z.string().array()
+    }),
+    category: z.enum(['island', 'temple', 'town']),
+    dateVisited: z.coerce.date(),
+    dateModified: z.coerce.date(),
+    datePublished: z.coerce.date()
+  })
+});
+
+export const collections = {
+  posts: postsCollection
+};
