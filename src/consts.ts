8a82dd0d14cbdf65a18c78a48d3b2d896596ba53
ymcheung <ymcheung.tw@me.com>
2024-12-29 15:49:20 +0800

    Add category path

--- src/consts.ts
+++ src/consts.ts
@@ -2,3 +2,14 @@ export const SITE_URL = 'https://meow.carrier.express';
 export const INTERSECTION = '𝙄 𝙉 𝙏 𝙀 𝙍 𝙎 𝙀 𝘾 𝙏 𝙄 𝙊 𝙉';
 export const INTERSECTION_DESCRIPTION =
   '優化、插件、高清、視頻、反饋、交互設計：已經看膩這些中國用語。';
+export const categories = [
+  {
+    slug: 'island'
+  },
+  {
+    slug: 'temple'
+  },
+  {
+    slug: 'town'
+  }
+];
