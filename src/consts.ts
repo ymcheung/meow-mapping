10fa93bea7a1efe6cff1be2394dc286b68dc0c00
ymcheung <ymcheung.tw@me.com>
2025-01-14 12:50:38 +0800

    Add SITE_DESCRIPTION

--- src/consts.ts
+++ src/consts.ts
@@ -3,7 +3,8 @@ import Temples from '@assets/temples.svg';
 
 export const SITE_URL = 'https://meow.carrier.express';
 export const SITE_NAME = 'Cat Travel Guides - Meow Mapping';
-export const SITE_DESCRIPTION = 'Cat Travel Guides - Meow Mapping';
+export const SITE_DESCRIPTION =
+  'Discover authentic travel experiences at some of the best cat spots, focusing on how to get to these destinations instead of just basic information. Primarily featured in Japan, these unique locales offer a chance to immerse yourself in the region with cats.';
 export const INTERSECTION = '𝙄 𝙉 𝙏 𝙀 𝙍 𝙎 𝙀 𝘾 𝙏 𝙄 𝙊 𝙉';
 export const INTERSECTION_DESCRIPTION =
   '優化、插件、高清、視頻、反饋、交互設計：已經看膩這些中國用語。';
