afd93ad74dfbc35584fb2d9388cc440b5c6b5177
ymcheung <ymcheung.tw@me.com>
2025-06-25 23:30:26 +0800

    Add redirects

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,6 +17,10 @@ export default defineConfig({
   markdown: {
     rehypePlugins: [rehypeUnwrapImages]
   },
+  redirects: {
+    '/streets': '/neighborhoods',
+    '/streets/[...blogPost]': '/neighborhoods/[...blogPost]'
+  },
   experimental: {
     fonts: [
       {
